9eb69824b25c09d3bd649494b385af62378b324c
pgw2006 <pgwbas@gmail.com>
2022-06-22 15:50:11 +0900

    update

---
+++ code2/code.py
@@ -0,0 +1,36 @@
+elif 4,5 <= score:
+    print("매우 만족")
+if not (score ==5):
+    print("5가 아니네")
+if score ==5 and score - 1 == 4:
+    print("점수는 5가 맞네")
+if score ==5:
+    print("완벽")
+
+alist[1, 2, 3, 4, 5]
+
+
+if 1 in alist:
+    print("1이 있내")
+
+if 99 not in alist:
+    print("99없음")
+
+print(alist[2])
+
+alist = alist + [1]
+alist = alist * 2
+alist.append(6)
+print(alist)
+
+del alist(3)
+alist.remove(2)
+alist.pop(4)
+alist.pop()
+print(alist)
+
+
+print(alist + [5])
+print(alist * 3)
+ for elelment in a:
+     print(elelment)
